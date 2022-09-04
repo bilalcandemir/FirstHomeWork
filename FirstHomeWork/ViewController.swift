19d7dd64aedd7be21e1d5d36ea0fd1e52fb2160f
Bilal Candemir <candemirbilal@yahoo.com>
2022-09-04 15:14:45 +0300

    Initial Commit

---
+++ FirstHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstHomeWork
+//
+//  Created by Bilal Candemir on 9/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
